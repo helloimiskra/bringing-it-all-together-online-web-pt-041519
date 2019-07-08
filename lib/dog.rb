c073e183a3ed47e7d432c96802a61bed65a034ab
Iskra B <ibatistapoblete@gmail.com>
2019-07-08 05:48:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,13 +11,11 @@ class Dog
 
   def self.create_table
     sql = "CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
-
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = "DROP TABLE dogs;"
-
     DB[:conn].execute(sql)
   end
 
