92d5ab26896f2d433448ce2940e962ad98aa41ef
Iskra B <ibatistapoblete@gmail.com>
2019-07-08 04:36:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,9 +36,9 @@ class Dog
     new_dog
   end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM dogs WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
     Dog.new(result[0], result[1], result[2])
   end
 
