2d6cf7251601caa9829e0bf4c857217e86a1d254
Iskra B <ibatistapoblete@gmail.com>
2019-07-08 05:17:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,8 @@ class Dog
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Dog.new(result[0], result[1], result[2])
+    dog = Dog.new(result[0], result[1], result[2])
+    dog
   end
 
   def self.create(name:, breed:)
@@ -53,7 +54,6 @@ class Dog
     dog.save
     dog
   end
-
 
 
   def update
